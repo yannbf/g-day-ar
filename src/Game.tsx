867bd5d24a962942ab9c49652356a697c6be2417
artempelenev <pstemka@gmail.com>
2020-01-16 15:32:21 +0100

    fixes for undroid setup

--- src/Game.tsx
+++ src/Game.tsx
@@ -7,7 +7,25 @@ import {
 } from 'react-viro';
 
 const Game: React.FC = () => {
-  return null;
+  return (
+    <ViroARScene>
+      <ViroAmbientLight color="#aaaaaa" />
+      <ViroBox
+        // dragType="FixedToWorld"
+        // onDrag={() => {}}
+        height={1}
+        length={1}
+        width={1}
+        position={[0, 0, -1]}
+        scale={[0.1, 0.1, 0.1]}
+        materials={['green']}
+        // physicsBody={{
+        //   type: 'Dynamic',
+        //   mass: 0.0001,
+        // }}
+      />
+    </ViroARScene>
+  );
 };
 
 ViroMaterials.createMaterials({
