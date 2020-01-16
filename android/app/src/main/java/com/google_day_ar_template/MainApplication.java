01f3ed6505bfacfaabc9e95fb7ae7b24674d133f
artempelenev <pstemka@gmail.com>
2020-01-16 13:29:04 +0100

    add typings, add andorid stuff

--- android/app/src/main/java/com/google_day_ar_template/MainApplication.java
+++ android/app/src/main/java/com/google_day_ar_template/MainApplication.java
@@ -2,6 +2,7 @@ package com.google_day_ar_template;
 
 import android.app.Application;
 import android.content.Context;
+import com.viromedia.bridge.ReactViroPackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -23,8 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf("GVR")));
           return packages;
         }
 
