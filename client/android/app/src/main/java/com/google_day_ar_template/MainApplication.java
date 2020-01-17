92b3ad9b46145b4a08b4ddd18f65e0377463cda0
jwfwessels <jwfwessels@gmail.com>
2020-01-17 18:22:58 +0100

    added initial web socket stuff

--- client/android/app/src/main/java/com/google_day_ar_template/MainApplication.java
+++ client/android/app/src/main/java/com/google_day_ar_template/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.viromedia.bridge.ReactViroPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
