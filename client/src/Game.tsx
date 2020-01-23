b4b728d2c2600631c79b17a2a7e6552c7e69f7c7
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2020-01-23 11:51:55 +0100

    chore: clean up the game file[1]

--- client/src/Game.tsx
+++ client/src/Game.tsx
@@ -1,108 +1,25 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React from 'react';
 import {
   ViroARScene,
   ViroAmbientLight,
   ViroBox,
   ViroMaterials,
-  ViroSphere,
-  ViroText,
 } from 'react-viro';
-import useWSServer from './useWSServer';
 import {StyleSheet} from 'react-native';
-import {getDeviceDetails} from './WebSocketController';
 
-const initialPosition = [Math.random() * 0.1, -1.5, -2];
+const initialPosition = [0, -0.5, -1];
+const baseDimensions = {
+  height: 0.1,
+  length: 0.3,
+  width: 0.3
+};
 
 const Game: React.FC = () => {
-  const {sendMessage, gameState} = useWSServer();
-  const [playerName, setPlayerName] = useState('');
-  const sphereInstance = useRef(null);
-  useEffect(() => {
-    getDeviceDetails().then(details => setPlayerName(details.deviceName));
-  }, []);
 
   return (
     <ViroARScene>
       <ViroAmbientLight color="#aaaaaa" />
-      {gameState?.started && !gameState?.done && (
-        <ViroSphere
-          viroTag="ball"
-          radius={0.15}
-          ref={sphereInstance}
-          position={[0, 1.5, -2]}
-          materials={['red']}
-          physicsBody={{
-            type: 'Dynamic',
-            mass: 0.3,
-            restitution: 1,
-          }}
-        />
-      )}
-
-      {gameState?.started && ( // scores
-        <>
-          <ViroText
-            width={1}
-            text="Score:"
-            position={[-1, 0.3, -3]}
-            style={styles.text}
-          />
-          {Object.entries(gameState?.players || {}).map(([key, value], index) => {
-            const {score, done, deviceName} = value;
-            return (
-              <ViroText
-                key={index}
-                width={1}
-                text={`${deviceName || key}: ${score}`}
-                // scale={[0.5, 0.5, 0.5]}
-                position={[-1, -0.5 * index, -3]}
-                style={[styles.text, {color: done ? 'red' : 'white'}]}
-              />
-            );
-          })}
-        </>
-      )}
-
-      {!gameState?.started && !gameState?.players[playerName] && (
-        <ViroText
-          text="Connect"
-          // scale={[0.5, 0.5, 0.5]}
-          position={[0, 0, -1]}
-          style={styles.text}
-          onClick={() => {
-            sendMessage({
-              connect: true,
-              deviceName: playerName,
-            });
-          }}
-        />
-      )}
-
-      {!gameState?.started && !!Object.keys(gameState?.players || {}).length && (
-        <ViroText
-          text="Start game"
-          position={[0, 0, -1]}
-          style={styles.text}
-          onClick={() => {
-            sendMessage({
-              gameStarted: true,
-            });
-          }}
-        />
-      )}
-
-      {gameState?.done && (
-        <ViroText
-          text="Restart"
-          position={[0, 0.2, -1]}
-          style={styles.text}
-          onClick={() => {
-            sendMessage({
-              restart: true,
-            });
-          }}
-        />
-      )}
+      
       <ViroBox
         dragType="FixedToPlane"
         dragPlane={{
@@ -110,16 +27,7 @@ const Game: React.FC = () => {
           planeNormal: [0, 1, 0],
           maxDistance: 10,
         }}
-        onCollision={() => {
-          sendMessage({
-            player: playerName,
-            hit: true,
-          });
-        }}
-        onDrag={() => {}}
-        height={0.1}
-        length={0.3}
-        width={0.3}
+        {...baseDimensions}
         position={initialPosition}
         materials={['green']}
         physicsBody={{
@@ -129,25 +37,6 @@ const Game: React.FC = () => {
             type: 'Sphere',
             params: [0.3],
           },
-        }}
-      />
-
-      <ViroBox
-        viroTag="ground"
-        onCollision={() => {
-          sendMessage({
-            player: playerName,
-            done: true,
-          });
-        }}
-        height={0.1}
-        length={50}
-        width={50}
-        position={[0, -2, -2.5]}
-        materials={['black']}
-        physicsBody={{
-          type: 'Static',
-          restitution: 0.5,
         }}
       />
     </ViroARScene>
